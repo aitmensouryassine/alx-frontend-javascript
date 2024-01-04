fd9f5fcf7f35cb01a7ffc3d45c2c440222e530da
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-01-04 15:56:29 +0000

    Task 8 improvement

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,6 +4,15 @@ export default class HolbertonClass {
     this._location = location;
   }
 
+  [Symbol.toPrimitive](cast) {
+    if (cast === 'string') {
+      return this._location;
+    } if (cast === 'number') {
+      return this._size;
+    }
+    return this;
+  }
+
   valueOf() {
     return this._size;
   }
