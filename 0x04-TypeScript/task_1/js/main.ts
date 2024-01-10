f359834770a96cb644f554ad7de70cd04f72f721
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2024-01-10 21:22:38 +0100

    Class inteface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// TASK 1
 interface Teacher {
 	readonly firstName: string;
 	readonly lastName: string;
@@ -7,10 +8,12 @@ interface Teacher {
 	[key: string]: any;
 }
 
+// TASK 2
 interface Directors extends Teacher {
 	numberOfReports: number;
 }
 
+// TASK 3
 interface printTeacherFunction {
 	(firstName: string, lastName: string): string;
 }
@@ -19,3 +22,33 @@ let printTeacher: printTeacherFunction;
 printTeacher = function (firstName, lastName) {
 	return firstName[0] + ". " + lastName;
 };
+
+// TASK 4
+interface StudentClassConstructorInterface {
+	new (firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	workOnHomework: () => string;
+	displayName: () => string;
+}
+
+class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework() {
+		return "Currently working";
+	}
+
+	displayName() {
+		return this.firstName;
+	}
+}
